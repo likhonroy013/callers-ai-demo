3051629505b1721a702ac3ad5a23630df39235e3
Likhon Roy <likhonroy@Likhons-MacBook-Pro.local>
2025-12-22 19:23:02 -0500

    Force new JS filename to bust cache

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/callers-ai-demo/',
-  build: { outDir: 'docs' },
+  build: {
+    outDir: 'docs',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-${Date.now()}.js`,
+        assetFileNames: `assets/[name]-${Date.now()}.[ext]`
+      }
+    }
+  },
   server: { port: 3000, open: true }
 })
